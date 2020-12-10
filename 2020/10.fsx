44cde6004e81c66ee73a8e7d333ad8cc1d7aa744
Jeremy Feinstein <jfeinstein@flatiron.com>
2020-12-10 08:18:12 -0500

    2020: 10

---
+++ 2020/10.fsx
@@ -0,0 +1,40 @@
+#! /usr/bin/env fsharpi
+
+open System.Collections.Generic
+
+let lines = 
+  System.IO.File.ReadLines("10.input")
+  |> Seq.toList
+  |> List.map (int)
+  |> List.sort
+
+let part1 lines =
+  let differences = lines |> List.mapi (fun idx value -> if idx = 0 then value else value - lines.[idx-1])
+  let differences1 = differences |> List.filter ((=) 1) |> List.length
+  let differences3 = differences |> List.filter ((=) 3) |> List.length
+  differences1 * (differences3 + 1)
+  
+printfn "Part 1: %i" (part1 lines)
+
+let part2 (lines:int list):float = 
+  let mutable memoize = Map.empty
+  let lastValue = List.max lines
+  let rec getPaths (lines:int list) (value:int):float =
+    if memoize.ContainsKey(value)
+    then memoize.[value]
+    else
+      if value = lastValue
+      then 1.0
+      else
+        let numPaths = 
+          [1..3] 
+          |> List.map (fun stepToNextValue -> 
+            match (List.tryFindIndex ((=) (value + stepToNextValue)) lines) with 
+            | Some nextIdx -> getPaths lines (value + stepToNextValue)
+            | None -> 0.0)
+          |> List.sum
+        memoize <- memoize.Add(value, numPaths)
+        numPaths
+  getPaths lines 0
+
+printfn "Part 2: %f" (part2 lines)
